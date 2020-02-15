01069c1b492262abcd33929be2d987c623b3c829
Dhosio <danielgithiomi@gmail.com>
2020-02-15 12:51:27 +0300

    user input accepted

--- js/index.js
+++ js/index.js
@@ -18,6 +18,27 @@ window.onclick = function(event) {
 
 function genderChange() {
 	var gender = [];
-	gender.push(document.getElementsByClassName("dropdown"));
+	gender.push(document.getElementsByClassName("dropdown-content"));
 	alert(gender);
+};
+
+function akanReveal() {
+	var dob = parseInt(document.getElementById("date").value);
+		if (dob < 0 || dob > 31){
+			alert("Enter a vaild date")
+		}
+	var mob = parseInt(document.getElementById("month").value);
+		if (mob < 0 || dob > 12){
+			alert("Enter a vaild month (00-12)");
+		}
+	var cob = parseInt(document.getElementById("century").value);
+		if (cob < 0 || dob > 20){
+			alert("Enter a vaild century (00-20)");
+		}
+	var yob = parseInt(document.getElementById("year").value);
+		if (yob < 0 || dob > 20){
+			alert("Enter a vaild year (00-20)");
+		}
+
+
 };
